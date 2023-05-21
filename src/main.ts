68f3ad7637b45ac413de9dd1b4e1bf18a8e21316
Maxime Petit <mpetithengen@gmail.com>
2023-05-21 13:53:48 +0200

    style: add .prettierrc

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,32 @@
-import { Command } from "commander";
+import { Command } from 'commander';
 
 const program = new Command();
 
 program
-  .version("0.0.1")
-  .description("raven-cli - a CLI tool to automate code reviews with GPT-4");
+  .version('0.0.1')
+  .description('raven-cli - a CLI tool to automate code reviews with GPT-4');
 
 program
-  .command("config")
-  .description("setup raven-cli")
+  .command('config')
+  .description('setup raven-cli')
   .action(() => {
-    console.log("Setup config");
+    console.log('Setup config');
   });
 
 program
-  .command("pr <repository> <pull_request>")
-  .description("review the specified pull request of the repository")
+  .command('pr <repository> <pull_request>')
+  .description('review the specified pull request of the repository')
   .action((repository: string, pull_request: string) => {
     console.log(
-      `Reviewing pull request ${pull_request} of repository ${repository}`
+      `Reviewing pull request ${pull_request} of repository ${repository}`,
     );
   });
 
 program
-  .command("local")
-  .description("review the local changes")
+  .command('local')
+  .description('review the local changes')
   .action(() => {
-    console.log("Reviewing local changes");
+    console.log('Reviewing local changes');
   });
 
 program.parse(process.argv);
--- src/services/config.ts
+++ src/services/config.ts
@@ -1,8 +1,8 @@
-import fs from "fs";
-import path from "path";
-import os from "os";
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
 
-const CONFIG_FILENAME = "rvn.json";
+const CONFIG_FILENAME = 'rvn.json';
 
 interface GithubConfig {
   githubApiUrl: string;
@@ -28,39 +28,39 @@ interface Config {
 class ConfigurationError extends Error {
   constructor(message: string) {
     super(message);
-    this.name = "ConfigurationError";
+    this.name = 'ConfigurationError';
   }
 }
 
 class ConfigService {
   private static getConfigPath(): string {
     const configDir =
-      process.env.NODE_ENV === "development"
+      process.env.NODE_ENV === 'development'
         ? process.cwd()
-        : path.join(os.homedir(), ".rvn");
+        : path.join(os.homedir(), '.rvn');
     return path.join(configDir, CONFIG_FILENAME);
   }
 
   private static getDefaultGithubConfig(): GithubConfig {
     return {
-      secretGithubToken: "ght_<your_token_here>",
-      githubApiUrl: "https://api.github.com",
+      secretGithubToken: 'ght_<your_token_here>',
+      githubApiUrl: 'https://api.github.com',
     };
   }
 
   private static getDefaultOpenAIConfig(): OpenAIConfig {
     return {
-      secretOpenaiApiKey: "sk-<your_key_here>",
+      secretOpenaiApiKey: 'sk-<your_key_here>',
       openaiTemperature: 0,
-      openaiApiUrl: "https://api.openai.com",
-      openaiModel: "gpt-3.5-turbo",
+      openaiApiUrl: 'https://api.openai.com',
+      openaiModel: 'gpt-3.5-turbo',
     };
   }
 
   private static validateTemperature(temperature: number): void {
     if (!(temperature >= 0.0 && temperature <= 2.0)) {
       throw new ConfigurationError(
-        "Invalid temperature value. It must be a value between 0 and 2 (inclusive)."
+        'Invalid temperature value. It must be a value between 0 and 2 (inclusive).',
       );
     }
   }
@@ -90,11 +90,11 @@ class ConfigService {
     const configPath = ConfigService.getConfigPath();
     if (!fs.existsSync(configPath)) {
       throw new ConfigurationError(
-        `Configuration file ${configPath} not found. Please run the config command.`
+        `Configuration file ${configPath} not found. Please run the config command.`,
       );
     }
 
-    const content = fs.readFileSync(configPath, "utf-8");
+    const content = fs.readFileSync(configPath, 'utf-8');
     const config: Config = JSON.parse(content);
 
     ConfigService.validateTemperature(config.llm.openai.openaiTemperature);
